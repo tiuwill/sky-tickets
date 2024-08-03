a57578a534461f88185d73e7ff7ba27a280909e4
Willian Moya <willian.fmoya@gmail.com>
2024-08-02 22:54:56 -0300

    Adapting dtos to java 22

--- sky-payments/src/main/java/br/com/springcloud/skytickets/payments/payment/PaymentRequest.java
+++ sky-payments/src/main/java/br/com/springcloud/skytickets/payments/payment/PaymentRequest.java
@@ -1,32 +1,3 @@
 package br.com.springcloud.skytickets.payments.payment;
 
-public class PaymentRequest {
-
-    private Long orderId;
-    private String cardNumber;
-    private Double amount;
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-}
+public record PaymentRequest(Long orderId, String cardNumber, Double amount) {}

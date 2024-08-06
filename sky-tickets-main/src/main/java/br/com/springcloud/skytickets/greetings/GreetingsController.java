e6a377b52e690619614e6d1355e99868160b982d
Willian Moya <willian.fmoya@gmail.com>
2024-08-05 23:11:41 -0300

    Adding logs

--- sky-tickets-main/src/main/java/br/com/springcloud/skytickets/greetings/GreetingsController.java
+++ sky-tickets-main/src/main/java/br/com/springcloud/skytickets/greetings/GreetingsController.java
@@ -1,5 +1,7 @@
 package br.com.springcloud.skytickets.greetings;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greetings")
 public class GreetingsController {
 
+    private static final Logger log = LoggerFactory.getLogger(GreetingsController.class);
+
     @Value("${hello.message}")
     private String message;
 
 
     @GetMapping
     public GreetingsDTO getGreetings() {
+        log.info("Calling greetings");
         return new GreetingsDTO(message);
     }
 
--- sky-tickets-order-manager/src/main/java/br/com/springcloud/skytickets/order/order/OrderController.java
+++ sky-tickets-order-manager/src/main/java/br/com/springcloud/skytickets/order/order/OrderController.java
@@ -4,6 +4,8 @@ package br.com.springcloud.skytickets.order.order;
 
 import br.com.springcloud.skytickets.order.order.repository.OrderRepository;
 import br.com.springcloud.skytickets.order.order.repository.domain.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrderController {
 
+    private Logger log = LoggerFactory.getLogger(OrderController.class);
+
     @Autowired
     private OrderRepository orderRepository;
 
     @PostMapping
     public Order createOrder(@RequestBody Order order) {
+        log.info("Creating Order");
         return orderRepository.save(order);
     }
 }

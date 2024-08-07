8fb90d71b00fb3769776047d93417673fc35db65
Willian Moya <willian.fmoya@gmail.com>
2024-08-06 23:22:14 -0300

    Reset main

--- servers/sky-tickets-config-server/src/main/java/br/com/springcloud/skytikects/configuration/SkyTicketsConfigServerApplication.java
+++ servers/sky-tickets-config-server/src/main/java/br/com/springcloud/skytikects/configuration/SkyTicketsConfigServerApplication.java
@@ -2,7 +2,6 @@ package br.com.springcloud.skytikects.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 public class SkyTicketsConfigServerApplication {
